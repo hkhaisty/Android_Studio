8d94f8bab16f10923fb20ba61d5aafdbe73c626c
hkhaisty <h.k.haisty@gmail.com>
2019-01-12 21:51:09 -0500

    adds dependencies

--- app/src/main/java/com/example/hkhai/myapplication/MainPage.java
+++ app/src/main/java/com/example/hkhai/myapplication/MainPage.java
@@ -2,6 +2,8 @@ package com.example.hkhai.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        EditText edit = (EditText)findViewById(R.id.editText);
+        TextView tview = (TextView)findViewById(R.id.textView);
+        String result = edit.getText().toString();
+        tview.setText(result);
     }
 }
