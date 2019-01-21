fcc9f4600c06e0ee0fef9b255116417052e9fefa
Harriet Haisty <hhaisty@students.kennesaw.edu>
2019-01-20 21:54:57 -0500

    finalizes design

--- Lab 02/app/src/main/java/com/example/harri/lab02/MainActivity.java
+++ Lab 02/app/src/main/java/com/example/harri/lab02/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.harri.lab02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        }
+
+
 }
