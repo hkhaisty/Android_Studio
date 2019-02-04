4368b7711e1e46b9acc313717b32c5def0fb7d10
hkhaisty <h.k.haisty@gmail.com>
2019-02-04 00:00:39 -0500

    adds three versions of lab four, none that work

---
+++ Lab4/app/src/main/java/com/example/lab4/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.lab4;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lab4.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+
+}
---
+++ Lab_04/app/src/main/java/lab_04/SecondActivity.java
@@ -0,0 +1,15 @@
+package lab_04;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.lab_04.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
