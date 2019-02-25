5be995124f161da4edca221de5cd68e52dc7b6c3
hkhaisty <h.k.haisty@gmail.com>
2019-02-24 22:17:22 -0500

    updates lab 5

--- Lab_05/app/src/main/java/com/example/lab_05/MainActivity.java
+++ Lab_05/app/src/main/java/com/example/lab_05/MainActivity.java
@@ -28,15 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick()
     {
-        if(user) {
-            Intent intent = new Intent(this, Activity_A.class);
-        }
-        else if () {
-            startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://developer.android.com")));
-        }
-        else {
-            Intent intent = new Intent(this, Activity_B.class);
-        }
+
+        Intent intent = new Intent(MainActivity.this, Activity_A.class);
+        Intent intent2 = new Intent(this, Activity_B.class);
+
+        startActivity(intent);
+        startActivity(intent2);
+//        if(user) {
+//            Intent intent = new Intent(this, Activity_A.class);
+//        }
+//        else if () {
+//            startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://developer.android.com")));
+//        }
+//        else {
+//            Intent intent = new Intent(this, Activity_B.class);
+//        }
 
     }
 
