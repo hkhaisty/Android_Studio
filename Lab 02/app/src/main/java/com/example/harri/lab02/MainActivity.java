c30d662c387083c28c185e85d85b05c6e340748a
Harriet Haisty <hhaisty@students.kennesaw.edu>
2019-04-10 15:45:45 -0400

    changes lab 2 dependencies

--- Lab 02/app/src/main/java/com/example/harri/lab02/MainActivity.java
+++ Lab 02/app/src/main/java/com/example/harri/lab02/MainActivity.java
@@ -15,6 +15,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         }
-
-
 }
---
+++ switchapp/app/src/main/java/com/example/switchapp/activityb.java
@@ -0,0 +1,13 @@
+package com.example.switchapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityb);
+    }
+}
